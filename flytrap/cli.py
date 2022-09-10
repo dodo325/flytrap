14469d7b4f317c1121587b70c3e93122273de9ee
dodo325 <dodo101000101@gmail.com>
2022-09-10 22:01:02 +0300

    feat: init CLI

--- flytrap/cli.py
+++ flytrap/cli.py
@@ -1,13 +1,44 @@
 import typer
-import uvicorn
+from typing import Optional
 
+import sys
+import signal
+
+from pyngrok import ngrok as ngrok_api
+
+from .app import app as web_app
 
 app = typer.Typer()
 
+_http_tunnel = None
+
+def handler(signal, frame):
+    print(' - CTRL-C pressed!')
+    if _http_tunnel is not None:
+        ngrok_api.disconnect(_http_tunnel.public_url)
+        _http_tunnel = None
+    sys.exit(0)
+
 
 @app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
+def base(
+    ngrok: bool = True,
+    port: int = 8080,
+    ngrok_token: Optional[str] = None
+    ):
+    # global _http_tunnel
+
+    # Start
+    # signal.signal(signal.SIGINT, handler)
+    # signal.pause()
+
+    # if ngrok_token:
+    #     ngrok_api.set_auth_token(ngrok_token)
+    # if ngrok:
+    #     _http_tunnel = ngrok_api.connect(port, "http")
+    #     typer.echo(f"HTTP tunnel: {_http_tunnel.public_url}")
+
+    web_app.run(host='0.0.0.0', port=port, debug=True)
 
 
 if __name__ == "__main__":
